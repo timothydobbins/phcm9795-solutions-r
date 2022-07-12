c641326d3dca44c3ac6ee9c5ac57376eb11563b4
Timothy Dobbins <t.dobbins@unsw.edu.au>
2022-07-13 09:45:12 +1000

    Begin Module 8

--- Module8-Activities.R
+++ Module8-Activities.R
@@ -1,19 +1,22 @@
-library(haven)
-library(tidyverse)
-library(skimr)
+iq <- readRDS("data/activities/Activity_S8.2.rds")
 
-iq <- read_stata("/Users/td/Library/CloudStorage/OneDrive-UNSW/Teaching/SPH Biostatistics Teaching/PHCM9795 - Foundations of Biostatistics/2022/5 - Activities/Datasets/Learning activity data/Activity_S8.2.dta")
-skim(iq)
+plot(iq$age, iq$iq, 
+     main="Scatter plot of IQ against age",
+     xlab = "Age (years)",
+     ylab = "IQ")
 
-plot(iq$age, iq$iq)
 abline(lm(iq$iq ~ iq$age))
-
-ggplot(iq, aes(x=age, y=iq)) +
-  geom_point() +
-  geom_smooth(method='lm', se=FALSE)
 
 cor.test(iq$age, iq$iq)
 
 model <- lm(iq$iq ~ iq$age)
 summary(model)
 confint(model)
+
+resids <- resid(model)
+
+hist(resids, probability =TRUE,
+     main = "Histogram of residuals")
+curve(dnorm(x,
+            mean=mean(resids),
+            sd=sd(resids)), add = TRUE)
